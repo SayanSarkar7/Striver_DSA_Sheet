1439c2fcefc5612290a7ab86b8b27d0910b2bdcb
SayanSarkar7 <sarkarsayan602@gmail.com>
2025-05-03 22:48:41 +0530

    adding bruteforce of kadans algorithm

---
+++ Arrays_1/4_kadans_Algorithm/bruteForce.js
@@ -0,0 +1,19 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var maxSubArray = function(nums) {
+    const n=nums.length;
+    let maxi = Number.MIN_SAFE_INTEGER;
+    let sum=0;
+    for(let i=0;i<n;i++){
+        sum+=nums[i];
+        if(sum>maxi){
+            maxi=sum;
+        }    
+        if(sum<0){
+            sum=0;
+        }
+    }
+    return maxi;
+};
