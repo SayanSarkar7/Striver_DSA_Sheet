f0e55c8f51bd68e3d2b952b2ffb3f11821b3a846
SayanSarkar7 <sarkarsayan602@gmail.com>
2025-05-05 05:36:15 +0530

    adding merge sort

---
+++ Arrays_1/5_sortColors/5.1_MergeSort.js
@@ -0,0 +1,37 @@
+/**
+ * @param {number[]} nums
+ * @return {void} Do not return anything, modify nums in-place instead.
+ */
+var sortColors = function(nums) {
+    function mergeSort(nums, low, high) {
+        if (low >= high) return;
+
+        const mid = Math.floor((low + high) / 2);
+        mergeSort(nums, low, mid);
+        mergeSort(nums, mid + 1, high);
+        merge(nums, low, mid, high);
+    }
+
+    function merge(nums, low, mid, high) {
+        const temp = [];
+        let left = low;
+        let right = mid + 1;
+
+        while (left <= mid && right <= high) {
+            if (nums[left] <= nums[right]) {
+                temp.push(nums[left++]);
+            } else {
+                temp.push(nums[right++]);
+            }
+        }
+
+        while (left <= mid) temp.push(nums[left++]);
+        while (right <= high) temp.push(nums[right++]);
+
+        for (let i = 0; i < temp.length; i++) {
+            nums[low + i] = temp[i];
+        }
+    }
+
+    mergeSort(nums, 0, nums.length - 1);
+};
