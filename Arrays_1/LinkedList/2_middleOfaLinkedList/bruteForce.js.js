9ba406c5d6349a2478aed36ca1efc415a634ad5f
SayanSarkar7 <sarkarsayan602@gmail.com>
2025-05-22 22:28:18 +0530

    update bruteForce approach of middle of a linked list

---
+++ Arrays_1/LinkedList/2_middleOfaLinkedList/bruteForce.js.js
@@ -0,0 +1,31 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+var middleNode = function(head) {
+
+    if(head===null){
+        return;
+    }
+    let curr=head;
+    let count=0;
+    while(curr!==null){
+        curr=curr.next;
+        count++;
+    }
+    curr=head;
+    for(let i=0;i<Math.floor(count/2);i++){
+        curr=curr.next;
+    }
+    return curr;
+
+
+    
+};
