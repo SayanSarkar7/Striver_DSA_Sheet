7b3a37eab00b4e951ceb5e784c5fa388c67c4469
SayanSarkar7 <sarkarsayan602@gmail.com>
2025-05-24 23:58:06 +0530

    update

---
+++ testCode.js
@@ -0,0 +1,15 @@
+// testing HashMap
+
+const map=new Map();
+map.set("sayan",25);
+map.set("nilanjana",18);
+map.set("priyani",17);
+console.log("map ---> ",map);
+
+const arr=Array.from(map);
+console.log("array ---> ",arr);
+
+
+
+
+
