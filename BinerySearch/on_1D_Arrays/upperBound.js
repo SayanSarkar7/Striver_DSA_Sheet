e4c450b51e16390308f7be0292beab08cabd44e5
SayanSarkar7 <sarkarsayan602@gmail.com>
2025-05-30 16:39:51 +0530

    update upper bound of binery search

---
+++ BinerySearch/on_1D_Arrays/upperBound.js
@@ -0,0 +1,26 @@
+// User function Template for javascript
+/**
+ * @param {number[]} arr
+ * @param {number} target
+ * @returns {number}
+    // t.c->O[log n]
+
+*/
+
+class Solution {
+    upperBound(arr, target) {
+        // your code here
+        let low=0,high=arr.length-1;
+        let ans=arr.length;
+        while(low<=high){
+            let mid=Math.floor((low+high)/2);
+            if(arr[mid]>target){
+                ans=mid;
+                high=mid-1;
+            }else{
+                low=mid+1;
+            }
+        }
+        return ans;
+    }
+}
