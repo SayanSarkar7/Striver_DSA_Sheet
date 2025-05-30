61da84c4667f2181a44c43f3b1fa92544c0dd30b
SayanSarkar7 <sarkarsayan602@gmail.com>
2025-05-30 19:18:27 +0530

    update search insert position of binery search

--- BinerySearch/on_1D_Arrays/lowerBound.js
+++ BinerySearch/on_1D_Arrays/lowerBound.js
@@ -1,28 +1,22 @@
-// User function Template for javascript
 /**
- * @param {number[]} arr
+ * @param {number[]} nums
  * @param {number} target
- * @returns {number}
+ * @return {number}
  */
+var searchInsert = function (nums, target) {
+  let low = 0,
+    high = nums.length - 1;
+  let ans = nums.length;
 
-class Solution {
-    lowerBound(arr, target) {
-        // your code here
-        let low=0,high=arr.length-1;
-        let ans=arr.length;
-        while(low<=high){
-            let mid=Math.floor((low+high)/2);
-            if(arr[mid]>=target){
-                ans=mid;
-                high=mid-1;
-            }
-            else{
-                low=mid+1;
-            }
-            
-        }
-        return ans;
-        
-        
+  while (low <= high) {
+    let mid = Math.floor((low + high) / 2);
+    // implementing lower bound
+    if (nums[mid] >= target) {
+      ans = mid;
+      high = mid - 1;
+    } else {
+      low = mid + 1;
     }
-}
+  }
+  return ans;
+};
