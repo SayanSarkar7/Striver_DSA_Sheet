38844a053cebb5c49a87fc984b8c0343c43d054f
SayanSarkar7 <sarkarsayan602@gmail.com>
2025-06-01 12:39:41 +0530

    update first and last position of a element in a sorted array

---
+++ BinerySearch/on_1D_Arrays/find_1st_lst_position_of_Element.js
@@ -0,0 +1,22 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+    
+    BruteForce Approach -> T.C -> O[n]
+
+*/
+var searchRange = function(nums, target) {
+    let first=-1,last=-1;
+    for(let i=0;i<nums.length;i++){
+        if(nums[i]===target){
+            if(first===-1){
+                first=i;
+            }
+            last=i;
+        }
+    }
+
+    return [first,last];
+
+};
