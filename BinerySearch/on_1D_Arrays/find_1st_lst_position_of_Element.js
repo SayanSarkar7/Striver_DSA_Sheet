6fb0746d82e36131836bfe17ff40835b11d84f35
SayanSarkar7 <sarkarsayan602@gmail.com>
2025-06-02 22:22:39 +0530

    update first and last position of a element in a sorted array

--- BinerySearch/on_1D_Arrays/find_1st_lst_position_of_Element.js
+++ BinerySearch/on_1D_Arrays/find_1st_lst_position_of_Element.js
@@ -1,22 +1,70 @@
+// /**
+//  * @param {number[]} nums
+//  * @param {number} target
+//  * @return {number[]}
+
+//     BruteForce Approach -> T.C -> O[n]
+
+// */
+// var searchRange = function(nums, target) {
+//     let first=-1,last=-1;
+//     for(let i=0;i<nums.length;i++){
+//         if(nums[i]===target){
+//             if(first===-1){
+//                 first=i;
+//             }
+//             last=i;
+//         }
+//     }
+
+//     return [first,last];
+
+// };
+// ----------------------------------------------
+// Better approach with O[log n] time complexity
+// ----------------------------------------------
+
 /**
  * @param {number[]} nums
  * @param {number} target
  * @return {number[]}
-    
-    BruteForce Approach -> T.C -> O[n]
+ */
+var searchRange = function (nums, target) {
+  const lb = lowerBound(nums, target);
+  if (lb === nums.length || nums[lb] !== target) {
+    return [-1, -1];
+  } else {
+    return [lb, upperBound(nums, target) - 1];
+  }
+};
 
-*/
-var searchRange = function(nums, target) {
-    let first=-1,last=-1;
-    for(let i=0;i<nums.length;i++){
-        if(nums[i]===target){
-            if(first===-1){
-                first=i;
-            }
-            last=i;
-        }
+const lowerBound = function (nums, target) {
+  let low = 0,
+    high = nums.length - 1;
+  let ans = nums.length;
+  while (low <= high) {
+    let mid = Math.floor((low + high) / 2);
+    if (nums[mid] >= target) {
+      ans = mid;
+      high = mid - 1;
+    } else {
+      low = mid + 1;
     }
-
-    return [first,last];
-
+  }
+  return ans;
+};
+const upperBound = function (nums, target) {
+  let low = 0,
+    high = nums.length - 1;
+  let ans = nums.length;
+  while (low <= high) {
+    let mid = Math.floor((low + high) / 2);
+    if (nums[mid] > target) {
+      ans = mid;
+      high = mid - 1;
+    } else {
+      low = mid + 1;
+    }
+  }
+  return ans;
 };
