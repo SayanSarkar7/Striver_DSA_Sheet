87cea7e2d636bce25e4092911bc4efe314f9bac8
SayanSarkar7 <sarkarsayan602@gmail.com>
2025-06-04 01:17:21 +0530

    update

--- BinerySearch/on_1D_Arrays/search_Insert_Position.js
+++ BinerySearch/on_1D_Arrays/search_Insert_Position.js
@@ -5,17 +5,9 @@
  */
 var searchInsert = function (nums, target) {
   let low = 0,
-    high = nums.lengrth - 1;
+    high = nums.length - 1;
   let ans = nums.length;
 
-  while (low <= high) {
-    let mid = Math.floor((low + high) / 2);
-    if (nums[mid] === target) ans = mid;
-    else if (nums[mid] >= target) low = mid + 1;
-    else high = mid - 1;
-  }
-  if (ans !== nums.length) return ans;
-  (low = 0), (high = nums.length - 1);
   while (low <= high) {
     let mid = Math.floor((low + high) / 2);
     // implementing lower bound
