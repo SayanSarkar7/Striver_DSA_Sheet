8d48f31079eb7c1343191529a60acd80c3027e53
SayanSarkar7 <sarkarsayan602@gmail.com>
2025-06-03 19:23:07 +0530

    update first and last position of a element in a sorted array

--- BinerySearch/on_1D_Arrays/find_1st_lst_position_of_Element.js
+++ BinerySearch/on_1D_Arrays/find_1st_lst_position_of_Element.js
@@ -24,11 +24,11 @@
 // Better approach with O[log n] time complexity
 // ----------------------------------------------
 
-/**
- * @param {number[]} nums
- * @param {number} target
- * @return {number[]}
- */
+// --------------------------------------------------------------------
+
+
+/*
+
 var searchRange = function (nums, target) {
   const lb = lowerBound(nums, target);
   if (lb === nums.length || nums[lb] !== target) {
@@ -68,3 +68,61 @@ const upperBound = function (nums, target) {
   }
   return ans;
 };
+
+*/
+// --------------------------------------------------------------------
+
+// In case you cant use lower bound and upper bound concept then use only binery search
+
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+var searchRange = function(nums, target) {
+    const fst=first(nums,target);
+    if(fst===-1){
+        return [-1,-1];
+    }
+    const lst=last(nums,target);
+    return [fst,lst];
+
+};
+const first=function(nums,target){
+    let fst=-1;
+    let low=0,high=nums.length-1;
+    while(low<=high){
+        let mid=Math.floor((low+high)/2);
+        if(nums[mid]===target){
+            fst=mid;
+            high=mid-1;
+        }else if(nums[mid]>target){
+            high=mid-1;
+        }else{
+            low=mid+1;
+        }
+    }
+    return fst;
+}
+const last=function(nums,target){
+    let lst=-1;
+    let low=0,high=nums.length-1;
+    while(low<=high){
+        let mid=Math.floor((low+high)/2);
+        if(nums[mid]===target){
+            lst=mid;
+            low=mid+1;
+        }else if(nums[mid]>target){
+            high=mid-1;
+        }else{
+            low=mid+1;
+        }
+    }
+    return lst;
+}
+
+
+
+
+
+
